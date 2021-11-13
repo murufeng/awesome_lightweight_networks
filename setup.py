aed4959daa7e12cf6901f8fa509d42f0d8666c03
murufeng <2394849504@qq.com>
2021-11-13 17:21:08 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-  name = 'light-network',
+  name = 'lightnet',
   packages = find_packages(exclude=['examples']),
   version = '0.1.0',
   license='MIT',
