3aaaa163844ae7ab6bbf2ef9ebabe5b4e06911fb
murufeng <2394849504@qq.com>
2021-11-25 14:22:50 +0800

    Update __init__.py

--- light_cnns/__init__.py
+++ light_cnns/__init__.py
@@ -4,3 +4,4 @@ from light_cnns.AdderNet import *
 from light_cnns.MobileNet import *
 from light_cnns.ShuffleNet import *
 from light_cnns.Attention import *
+from light_cnns.mobile_cpu_network import *
