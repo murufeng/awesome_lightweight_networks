81adaf3013722cce57c5240d77b44362efe50f4c
murufeng <2394849504@qq.com>
2021-12-09 23:08:49 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'light_cnns',
   packages = find_packages(),
-  version = '0.3.0',
+  version = '0.4.0',
   license='MIT',
   description = 'Implementation of Lightweight Network in Pytorch',
   author = 'murufeng',
