3519b82419141c81fb15dbddff56e0063507debd
murufeng <2394849504@qq.com>
2021-12-14 19:59:47 +0800

    Update cls_hrnet.py

--- light_cnns/HRNet/cls_hrnet.py
+++ light_cnns/HRNet/cls_hrnet.py
@@ -1,10 +1,3 @@
-# ------------------------------------------------------------------------------
-# Copyright (c) Microsoft
-# Licensed under the MIT License.
-# Written by Bin Xiao (Bin.Xiao@microsoft.com)
-# Modified by Ke Sun (sunk@mail.ustc.edu.cn)
-# ------------------------------------------------------------------------------
-
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
